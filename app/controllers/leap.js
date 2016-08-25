2b596a2f3fc709796ce844c176a2c431e8b0df32
Calieston <calieston@hotmail.com>
2016-08-26 00:56:04 +0200

    Some refactoring

--- app/controllers/leap.js
+++ app/controllers/leap.js
@@ -3,12 +3,17 @@
 var leapjs = require('leapjs');
 var log4js = require('log4js');
 var leap;
-var bool = true;
 var socketCtrl = require('./socket');
+
+// flag variable to set timeout
+var bool = true;
 var logger = log4js.getLogger("leapclient");
+
+// gesture constants
 const gestureAction = 'gesture';
-const recordAction= 'record';
+const recordAction = 'record';
 const screenTap = 'screenTap';
+const keyTap = 'keyTap';
 
 // avoid multiple gesture detection with timeout
 function timeout() {
@@ -19,210 +24,121 @@ function timeout() {
   }, 1000);
 }
 exports.initialize = (params) => {
-  var widgets = params.gestureWidgets;
-  var newList = [];
-  var widgetList = [];
-  widgets.forEach(function(item) {
-    var widget = {};
-    widget.name = item.module.name;
-    widget.gesture = item.gesture.gestureType;
-    widgetList.push(widget);
-  });
-  validateAndProcessGesture(widgetList, 'swipe left', gestureAction);
-  // intialize leap motion controller
-  var leap = new leapjs.Controller({
-    enableGestures: true
-  });
+    var widgets = params.gestureWidgets;
+    var newList = [];
+    var widgetList = [];
+    widgets.forEach(function(item) {
+      var widget = {};
+      widget.name = item.module.name;
+      widget.gesture = item.gesture.gestureType;
+      widgetList.push(widget);
+    });
+    // timeout for testing
+    setTimeout(function() {
+      validateAndProcessGesture(widgetList, 'swipe left', gestureAction);
+    }, 5000);
+    // intialize leap motion controller
+    var leap = new leapjs.Controller({
+      enableGestures: true
+    });
 
-  leap.on('error', function() {
-    logger.error('error');
-  })
+    leap.on('error', function() {
+      logger.error('error');
+    })
 
-  leap.connect();
-  leap.on('deviceFrame', function(frame) {
+    leap.connect();
+    leap.on('deviceFrame', function(frame) {
 
-    // Loop through available gestures
-    for (var i = 0; i < frame.gestures.length; i++) {
-      var gesture = frame.gestures[i];
-      switch (gesture.type) {
-        case screenTap:
-          // gesture for play module
-          if (gesture.state == 'stop') {
-            validateAndProcessGesture(widgetList, screenTap, gestureAction);
-          }
-          break;
-        case 'circle':
-          // gesture for play module
-          if (gesture.state == 'stop') {
+      // Loop through available gestures
+      for (var i = 0; i < frame.gestures.length; i++) {
+        var gesture = frame.gestures[i];
+        switch (gesture.type) {
+          case screenTap:
+            // gesture for play module
+            if (gesture.state == 'stop') {
+              validateAndProcessGesture(widgetList, screenTap, gestureAction);
+            }
+            break;
+          case keyTap:
+            // gesture for play module
+            if (gesture.state == 'stop') {
+              validateAndProcessGesture(widgetList, keyTap, recordAction);
+            }
+          case 'circle':
+            // gesture for play module
+            if (gesture.state == 'stop') {
               var isClockwise = (gesture.normal[2] <= 0);
               if (isClockwise) {
                 logger.info('circle clockwise');
-                validateAndProcessGesture(widgetList, 'circle clock', recordAction);
+                validateAndProcessGesture(widgetList, 'circle clock', gestureAction);
               } else {
                 logger.info('circle unclock');
-                //validateAndProcessGesture(widgetList, 'circle clock', 'record');
+                validateAndProcessGesture(widgetList, 'circle clock', gestureAction);
               }
-          }
-          break;
+            }
+            break;
+          case 'swipe':
+            //Classify swipe as either horizontal or vertical
+            var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
+            if (isHorizontal) {
+              if (gesture.direction[0] > 0) {
+                // SWIPE RIGHT
+                if (gesture.state == 'stop') {
+                  validateAndProcessGesture(widgetList, 'swipe right', gestureAction);
+                }
+              } else {
+                // SWIPE LEFT
+                if (gesture.state == 'stop') {
+                  validateAndProcessGesture(widgetList, 'swipe left', gestureAction);
+                }
+              }
+            } else {
+              //vertical
+              if (gesture.direction[1] > 0) {
+                // SWIPE UP
+                if (gesture.state == 'stop') {
+                  validateAndProcessGesture(widgetList, 'swipe up', gestureAction);
+                }
+              } else {
+                // SWIPE DOWN
+                if (gesture.state == 'stop') {
+                  validateAndProcessGesture(widgetList, 'swipe down', gestureAction);
+                }
+              }
+            }
+            break;
+        }
       }
-    }
-  });
-}
-// check if recognized gesture is available in user widgets gesture
+    });
+  }
+  // check if recognized gesture is available in user widgets gesture
 function validateAndProcessGesture(list, gestureType, action) {
   if (bool == true) {
-    switch(action) {
+    switch (action) {
       case 'gesture':
-        list.forEach(function(item){
-          if(item.gesture == gestureType){
-            logger.debug('Found gesture match: '+gestureType);
+        // iterate over all gesture widgets
+        list.forEach(function(item) {
+          if (item.gesture == gestureType) {
+            logger.debug('Found gesture match: ' + gestureType);
             let params = {};
             params.widget = item.name,
-            params.gesture = item.gesture,
-            params.action = action
+              params.gesture = item.gesture,
+              params.action = action
             socketCtrl.sendMessage(params);
             timeout();
-          }else{
-            logger.debug('gesture '+gestureType + ' not found in user widget '+ item.name);
+          } else {
+            logger.debug('gesture ' + gestureType + ' not found in user widget ' + item.name);
           }
         });
-      break;
+        break;
       case 'record':
-            let params = {};
-            params.action = action
-            socketCtrl.sendMessage(params);
-            timeout();
-      break;
+        let params = {};
+        params.action = action
+        socketCtrl.sendMessage(params);
+        timeout();
+        break;
     }
   }
 }
 
-/*leap.on('deviceFrame', function(frame) {
-  if (frame.hands.length > 0) {
-    var hand = frame.hands[0];
-    var timeVisible = hand.timeVisible;
-    if (timeVisible > 5) {
-      // detect if hand is visible more than 5 seconds
-      // console.log('hand visible');
-    }
-    var finger = hand.finger(hand.thumb);
-    //console.log('FINGER'+ finger);
-  }
-  // Loop through available gestures
-  for (var i = 0; i < frame.gestures.length; i++) {
-    var gesture = frame.gestures[i];
-    switch (gesture.type) {
-      case 'circle':
-        // gesture for play module
-        if (gesture.state == 'stop') {
-          if (bool == true) {
-            var isClockwise = (gesture.normal[2] <= 0);
-            if (isClockwise) {
-              console.log('circle clockwise');
-              socket.emit('smartmirror', {
-                msg: 'record'
-              });
-            } else {
-              console.log('circle counterclockwise');
-              socket.emit('smartmirror', {
-                msg: ''
-              });
-            }
-            var circleProgress = gesture.progress;
-            var completeCircles = Math.floor(circleProgress);
-            if (completeCircles > 0) {
-              socket.emit('smartmirror', {
-                msg: ''
-              });
-            }
-            timeout();
-          }
-        }
-        break;
-      case 'screenTap':
-        // gesture for play module
-        if (gesture.state == 'stop') {
-          if (bool == true) {
-            console.log('screen tap');
-            socket.emit('smartmirror', {
-              msg: ''
-            });
-            timeout();
-          }
-        }
-        break;
-      case 'handClosed':
-        console.log('hand closed');
-        break;
-      case 'keyTap':
-        // gesture for play module
-        if (gesture.state == 'stop') {
-          if (bool == true) {
-            console.log('tap');
-            socket.emit('smartmirror', {
-              msg: 'tagesschau'
-            });
-            timeout();
-          }
-        }
-        break;
-      case 'swipe':
-        //Classify swipe as either horizontal or vertical
-        var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
-        if (isHorizontal) {
-          if (gesture.direction[0] > 0) {
-            // SWIPE RIGHT
-            if (gesture.state == 'stop') {
-              if (bool == true) {
-                swipeDirection = "right";
-                console.log('right');
-                socket.emit('smartmirror', {
-                  msg: 'record'
-                });
-                timeout();
-              }
-            }
-          } else {
-            // SWIPE LEFT
-            if (gesture.state == 'stop') {
-              if (bool == true) {
-                swipeDirection = "left";
-                console.log('left');
-                socket.emit('smartmirror', '');
-                timeout();
-              }
-            }
-          }
-        } else {
-          //vertical
-          if (gesture.direction[1] > 0) {
-            if (gesture.state == 'stop') {
-              // SWIPE UP
-              if (bool == true) {
-                console.log("up");
-                swipeDirection = "up";
-                socket.emit('smartmirror', '');
-                timeout();
-              }
-            }
-          } else {
-            if (gesture.state == 'stop') {
-              // SWIPE DOWN
-              swipeDirection = "down";
-              if (bool == true) {
-                console.log("down");
-                swipeDirection = "down";
-                socket.emit('smartmirror', '');
-                bool = false;
-                setTimeout(function() {
-                  bool = true;
-                }, 2000);
-              }
-            }
-          }
-        }
-        break;
-    }
-  }
-});*/
 exports.leap = leap;
--- app/controllers/socket.js
+++ app/controllers/socket.js
@@ -4,36 +4,38 @@ var log4js = require('log4js');
 var io = require('socket.io-client');
 var leapCtrl = require('./leap');
 const ip = 'http://localhost';
+//const ip = 'http://141.62.54.107';
 const port = '3000';
 var logger = log4js.getLogger("leapclient");
 var socket = io.connect(ip + ':' + port, {
   reconnect: true
 });
+
 // add a connect listener
 socket.on('connect', function(socket) {
   logger.info('Connected successfully to smartmirror');
-  logger.debug('IP: '+ip + ' Port: '+ port);
+  logger.debug('IP: ' + ip + ' Port: ' + port);
 });
-socket.on('smartmirror-weather', function(data){
-  logger.info('RECEIVED MESSAGE: '+ JSON.stringify(data, null, 4));
+socket.on('smartmirror-weather', function(data) {
+  logger.info('RECEIVED MESSAGE: ' + JSON.stringify(data, null, 4));
 })
 socket.on('gestures', function(data) {
   logger.info('received widget data from mirror');
-  logger.debug('mirror data: '+ JSON.stringify(data, null, 4));
+  logger.debug('mirror data: ' + JSON.stringify(data, null, 4));
   let params = {};
-  if(data.userGestureWidgets != null){
-  logger.debug('Gesture widgets found. Start initialization!')
-  params.gestureWidgets = data.userGestureWidgets;
-  leapCtrl.initialize(params);
-  }else{
+  if (data.userGestureWidgets != null) {
+    logger.debug('Gesture widgets found. Start initialization!')
+    params.gestureWidgets = data.userGestureWidgets;
+    leapCtrl.initialize(params);
+  } else {
     logger.debug('no gesture widgets found!');
   }
 });
 
 exports.sendMessage = (params) => {
   logger.info('Send message to smart mirror');
-  logger.debug('socket message: '+ JSON.stringify(params, null, 4));
-  socket.emit('leap client', params);
+  logger.debug('socket message: ' + JSON.stringify(params, null, 4));
+  socket.emit('smartmirror', params);
 }
 
 exports.io = io;
